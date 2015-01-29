14f1d82a3e5335df26ebed268786fa61e8638428
foghr <antti.kotiranta@cs.helsinki.fi>
2015-01-29 17:50:20 +0200

    v3pakolliset

--- ratebeer/app/controllers/ratings_controller.rb
+++ ratebeer/app/controllers/ratings_controller.rb
@@ -10,18 +10,21 @@ class RatingsController < ApplicationController
   end
   
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
 
-    # talletetaan tehdyn reittauksen sessioon
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-
-    redirect_to ratings_path
+    if @rating.save
+      current_user.ratings << @rating
+      redirect_to user_path current_user
+    else
+      @beers = Beer.all
+      render :new
+    end
   end
 
    def destroy
     rating = Rating.find(params[:id])
-    rating.delete
-    redirect_to ratings_path
+    rating.delete if current_user == rating.user
+    redirect_to :back
   end
 
 
